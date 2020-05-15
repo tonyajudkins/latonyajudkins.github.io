ed332c086728d5ed415048b7d0df196754d69f46
latonyajudkins <ljudkins@icstars.org>
2020-05-14 20:30:48 -0500

    Updates

---
+++ precache-manifest.989404536db39e0888e422ed635ff555.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e794e1742212a7b5b7c67360f7a1d3e",
+    "url": "/latonyajudkins.github.io/index.html"
+  },
+  {
+    "revision": "904304c4233158732439",
+    "url": "/latonyajudkins.github.io/static/css/main.f6cffc85.chunk.css"
+  },
+  {
+    "revision": "c3ce85ddc74af9c74dcf",
+    "url": "/latonyajudkins.github.io/static/js/2.c0343868.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/latonyajudkins.github.io/static/js/2.c0343868.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "904304c4233158732439",
+    "url": "/latonyajudkins.github.io/static/js/main.47565533.chunk.js"
+  },
+  {
+    "revision": "3fe92d8ab0d96f0f4c95",
+    "url": "/latonyajudkins.github.io/static/js/runtime-main.107d0f11.js"
+  }
+]);
