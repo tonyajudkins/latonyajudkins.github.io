0d2aca37d153bc3360866699897f750a29777aed
latonyajudkins <ljudkins@icstars.com>
2020-05-18 05:15:03 -0500

    girlCocder

---
+++ src/Tonya/Components/GirlCoder.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import girlCoder from '../../Assets/girlCoder.png';
+import me from "../../Assets/beingMe.jpg";
+
+
+const GirlCoder = () => {
+    return (
+      <div className={'girlCoder'}>
+        <img src={girlCoder} className={'girl'} alt='beingMe' width='100' height='120' />
+        <div className={'coder'}> {"< Girl Coder />"} </div>
+      </div>
+    );
+};
+
+export default GirlCoder;
