2d861f50f414e9e1b32c8eb56b8cbe9bb53d585a
latonyajudkins <ljudkins@icstars.com>
2020-09-09 22:50:29 -0500

    Update TechLife.js

--- src/Tonya/Components/TechLife.js
+++ src/Tonya/Components/TechLife.js
@@ -12,7 +12,7 @@ const TechLife = () =>{
           Today's workflow consist of backend development, infrastructure, database, and cloud computing.
         </div>
         <div className={'life'} >
-          My journey into technology all started at a non-profit organization called i.c.stars.  Their focus is creating leaders in business and technology.  During my internship I meet a lot of people.  One of the most motivating things that I experienced was the women who were advocating for women in tech and other engineering that are men dominated.  It was an empowering experience that boosted my confidence and eased my fear of failing
+          My journey into technology all started at a non-profit organization called i.c.stars.  Their focus is creating leaders in business and technology.  During my internship I meet a lot of people.  One of the most motivating things that I have experienced was the women who were advocating for women in tech and other engineering that are men dominated.  It was an empowering experience that boosted my confidence and eased my fear of failing
         </div>
         <div className={'life'} >
           I am a huge advocate for <span className={'wit'}>WOMEN IN TECHNOLOGY</span> and diversity and inclusion.
